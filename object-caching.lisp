f35ddf3881a9b7ed4501f3c75feff26053815155
Wilfried Meindl <wilfried.meindl@gmail.com>
2013-11-12 17:49:27 +0100

    setters and getters for cached objects

--- object-caching.lisp
+++ object-caching.lisp
@@ -2,7 +2,7 @@
 
 (defclass object-caching-mixin ()
   ((object-cache :reader object-cache
-		:initform (make-hash-table :test 'equal))))
+		 :initform (make-hash-table :test 'equal))))
 
 (defgeneric update-records-from-cache (database)
   (:method ((database object-caching-mixin))
@@ -13,3 +13,10 @@
 
 (defmethod close-database :before ((database object-caching-mixin))
   (update-records-from-cache database))
+
+(defun cached-object (values database)
+  (gethash values (object-cache database)))
+
+(defun (setf cached-object) (object values database)
+  (setf (gethash values (object-cache database)) object))
+
