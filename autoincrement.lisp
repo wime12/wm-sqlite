a847891ed5e86f1a259104c0cef6aed6e72c462c
Wilfried Meindl <wilfried.meindl@gmail.com>
2013-11-17 21:42:51 +0100

    correct checking if autoincrement-mixin can be used.

--- autoincrement.lisp
+++ autoincrement.lisp
@@ -7,14 +7,18 @@
   (let* ((primary-key (primary-key class))
 	 (primary-key-length (length primary-key)))
     (when (zerop primary-key-length)
-      (error "No primary key defined for ~S: cannot use autoincrement-mixin."
+      (error "Cannot use autoincrement-mixin: no primary key defined for ~S."
 	     class))
     (when (> primary-key-length 1)
-      (error "Primary key of ~S does not consist of a single slot: Cannot use autoincrement-mixin." class))
+      (error "Cannot use autoincrement-mixin: primary key of ~S does not consist of a single slot." class))
     (unless (eq (sqlite-persistent-slot-definition-persistence
 		 (find-slotd class (car primary-key)))
 		:integer)
-      (error "Primary key slot of ~S is not declared to have type :INTEGER: Cannot use autoincrement-mixin." class))))
+      (error "Cannot use autoincrement-mixin: primary key slot of ~S is not declared to have type :INTEGER." class))
+    (unless (eq (sqlite-persistent-slot-definition-primary-key
+		 (find-slotd class (car (primary-key class))))
+		:autoincrement)
+      (error "Cannot use autoincrement-mixin: primary key slot of ~S is not declared as :autoincrement." class))))
 
 (defmethod insert-record ((database database) (instance autoincrement-mixin))
   (call-next-method)
